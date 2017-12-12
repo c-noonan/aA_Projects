7d09fc4be5e9398148b324c6726c95f81244a50f
Katie Noonan <catherine.c.noonan@gmail.com>
2017-12-12 09:38:40 -0800

    Create Memory game and start HTML practice

---
+++ W1D2/test.rb
@@ -0,0 +1,22 @@
+#
+#
+# board.[](0,0)
+#
+# user input: [0, 0]
+#
+# board[[0,0]]
+# vs
+# board.grid[0][0]
+#
+# board[[0,0]] = "x"
+# board.[[0,0]]="x"
+
+class Array
+
+  def [](arg)
+    puts "index"
+  end
+end
+
+arr = [1, 2, 3]
+arr[0]
